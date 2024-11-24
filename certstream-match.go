6b7d0b8d9538a496a1f2407987aff8a43060ab81
x123 <x123@users.noreply.github.com>
2024-11-24 20:57:54 +0100

    regexp matcher

--- certstream-match.go
+++ certstream-match.go
@@ -6,18 +6,55 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
+var stringarray []string
+
 func main() {
-	echo()
+	regexString := loadRegex()
+	fmt.Printf("regex_string:%s\n", regexString)
+	var re = regexp.MustCompile(regexString)
+	echoMatchedStdin(re)
 }
 
-func echo() error {
-	// This function just echos any stdin.
+func loadRegex() string {
+	f, err := os.Open("./regexs.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var array []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		match, _ := regexp.MatchString("^#", scanner.Text())
+		if !match {
+			array = append(array, scanner.Text())
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var sb strings.Builder
+	sb.WriteString("(")
+	sb.WriteString(strings.Join(array, "|"))
+	sb.WriteString(")")
+	return sb.String()
+}
+
+func echoMatchedStdin(re *regexp.Regexp) error {
+	// This function just echos any stdin that matches regexp re
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		match := re.MatchString(scanner.Text())
+		if match {
+			fmt.Println(scanner.Text())
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		return err
